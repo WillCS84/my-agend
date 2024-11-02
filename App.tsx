ba07015238607e1350f9d6c3253941cb8b986871
Willian-CS <will.cesar.84@gmail.com>
2024-11-02 19:25:11 -0300

    commit initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import AgendaScreen from "./src/screens/agendaScreen"
+import AppointmentScreen from "./src/screens/settingsScreens"
+import SettingsScreen from "./src/screens/appointmentScreen"
+
+type SettingsType = {
+  notificationsEnabled: boolean
+  reminderTime: number
+}
+
+export type RootStackParamList = {
+  Agenda: undefined
+  "Novo Agendamento": { appointmentId?: string }
+  "Configurações de Horário": { settings: SettingsType }
+}
+
+const Stack = createStackNavigator<RootStackParamList>()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Agenda">
+        <Stack.Screen name="Agenda" component={AgendaScreen} />
+        <Stack.Screen name="Novo Agendamento" component={SettingsScreen} />
+        <Stack.Screen name="Configurações de Horário" component={AppointmentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/global.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  buttonPlus: {}
+})
